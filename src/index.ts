17dbbd998cfabf4962e4fab223909f863f35d14a
jonocairns <jonocairns@gmail.com>
2017-07-08 18:27:26 +1200

    adding scheduler

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,50 @@
-import { MessageEvent, ReactionEvent } from 'slack';
-
-const blah = require('dotenv').config();
-const RtmClient = require('@slack/client').RtmClient;
+import { MessageEvent, ReactionEvent, RtmClient } from 'slack';
+require('dotenv').config();
+const CLIENT = require('@slack/client').RtmClient;
+const rtmEvents = require('@slack/client').RTM_EVENTS;
 const CLIENT_EVENTS = require('@slack/client').CLIENT_EVENTS;
-const RTM_EVENTS = require('@slack/client').RTM_EVENTS;
-const MemoryDataStore = require('@slack/client').MemoryDataStore;
+const schedule = require('node-schedule');
+const WebClient = require('@slack/client').WebClient;
 
 const token = process.env.SLACK_API_TOKEN || '';
-const rtm = new RtmClient(token);
+const rtm: RtmClient = new CLIENT(token);
 rtm.start();
 
-rtm.on(RTM_EVENTS.MESSAGE, (message: MessageEvent) => {
+let channel = '';
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, (rtmStartData: any) => {
+  for (const c of rtmStartData.channels) {
+    console.log(c);
+    if (c.is_member && c.name === 'general') {
+      console.log(`channel ID: ${c.id}`);
+      channel = c.id;
+    }
+  }
+  console.log(`Logged in as ${rtmStartData.self.name} of team ${rtmStartData.team.name}, but not yet connected to a channel`);
+});
+
+schedule.scheduleJob('*/10 * * * * *', () => {
+  const web = new WebClient(token);
+  web.chat.postMessage(channel, 'Its that time already...', (err: any, res: any) => {
+    if (err) {
+      console.log('Error:', err);
+    } else {
+      // console.log('Message sent: ', res);
+    }
+  });
+});
+
+rtm.on(rtmEvents.MESSAGE, (message: MessageEvent) => {
   console.log(message);
 
   rtm.sendMessage('I got your message', message.channel);
 });
 
-rtm.on(RTM_EVENTS.REACTION_ADDED, (reaction: ReactionEvent) => {
+rtm.on(rtmEvents.REACTION_ADDED, (reaction: ReactionEvent) => {
   console.log('Reaction added:', reaction);
 });
 
-rtm.on(RTM_EVENTS.REACTION_REMOVED, (reaction: ReactionEvent) => {
+rtm.on(rtmEvents.REACTION_REMOVED, (reaction: ReactionEvent) => {
   console.log('Reaction removed:', reaction);
 });
 
