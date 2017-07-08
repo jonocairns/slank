47fdbef6ab14609ef453f71c2f4a4a4a52f797b0
jonocairns <jonocairns@gmail.com>
2017-07-08 16:18:28 +1200

    getting started

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,14 @@
-console.log('hello world');
+require('dotenv').config();
+var WebClient = require('@slack/client').WebClient;
 
-const blah = 'hi';
+var token = process.env.SLACK_API_TOKEN || '';
+console.log(token);
+
+var web = new WebClient(token);
+web.chat.postMessage('general', 'Hello there', (err: Error, res: any) => {
+    if (err) {
+        console.log('Error:', err);
+    } else {
+        console.log('Message sent: ', res);
+    }
+});
